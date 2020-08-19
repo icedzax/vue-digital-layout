25e19722446ae09bca5fbbf9f463cbfc026338b9
jannarong.j <icedza@gmail.com>
2020-08-19 09:08:13 +0700

    change route home page to DL page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+//import Home from "../views/Home.vue";
 
 import Grid from "../views/Grid.vue";
 
@@ -10,7 +10,7 @@ const routes = [
   {
     path: "/",
     name: "Home",
-    component: Home
+    component: Grid
   },
   {
     path: "/dev",
